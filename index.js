87b1d64d1b32849ccd5589a1c3eca7d767d4a365
benjadelcampo15 <benjadelcampo15@gmail.com>
2024-03-12 13:38:30 -0300

    phone

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+console.log("hola");
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('a[href^="#"]');
+    
+    links.forEach(link => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            
+            const targetId = this.getAttribute("href");
+            const targetElement = document.querySelector(targetId);
+            
+            if (targetElement) {
+                const offset = targetElement.offsetTop - 50; // Ajusta este valor según sea necesario
+                window.scrollTo({
+                    top: offset,
+                    behavior: "smooth"
+                });
+            }
         });
     });
 });
